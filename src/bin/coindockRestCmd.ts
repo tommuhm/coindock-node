7c9b1cdd6f02c7040c5786940527cd5a264bfa40
Tom Muhm <tom.muhm@gmail.com>
2019-03-14 11:12:01 +0100

    fix argv check

--- src/bin/coindockRestCmd.ts
+++ src/bin/coindockRestCmd.ts
@@ -5,10 +5,8 @@ import minimist = require('minimist');
 
 const argv = minimist(process.argv.slice(2));
 
-if (argv._length < 4 || argv._length > 6) {
-  console.log(`usage: coindockRestCmd endpoint exchange symbol interval [--from=timestampMs] [--to=timestampMs] [--limit=number] [openLimit=number]`);
-  console.log(`example: coindockRestCmd localhost:5555 binance btcusdt 5min --from=1547392298000 --limit=500 --openLimit=30`);
-  process.exit(1);
+if (argv._.length < 4 || argv._.length > 6) {
+  usage();
 }
 
 
@@ -21,6 +19,11 @@ const from = argv['from'] != null ? Number(argv['from']) : undefined;
 const to = argv['to'] != null ? Number(argv['to']) : undefined;
 const openLimit = argv['openLimit'] != null ? Number(argv['openLimit']) : undefined;
 
+if (endpoint == null || exchange == null || symbol == null || interval == null) {
+  usage();
+}
+
+
 const coindock = new CoindockRest({endpoint});
 
 console.log(`using: ${endpoint}, sending: exchange=${exchange}, symbol=${symbol}, interval=${interval}, limit=${limit}, from=${from}, to=${to}, openLimit=${openLimit}`);
@@ -31,3 +34,9 @@ coindock.ohlcv({exchange, symbol, interval, limit, from, to, openLimit}, (err, r
   }
   console.log(response);
 });
+
+function usage() {
+  console.log(`usage: ./coindock-rest endpoint exchange symbol interval [--from=timestampMs] [--to=timestampMs] [--limit=number] [openLimit=number]`);
+  console.log(`example: ./coindock-rest localhost:5555 binance btcusdt 5min --from=1547392298000 --limit=500 --openLimit=30`);
+  process.exit(1);
+}
--- src/bin/coindockWsCmd.ts
+++ src/bin/coindockWsCmd.ts
@@ -5,10 +5,8 @@ import minimist = require('minimist');
 
 const argv = minimist(process.argv.slice(2));
 
-if (argv._length < 4 || argv._length > 6) {
-  console.log(`usage: coindockWsCmd endpoint exchange symbol interval  [limit=number] [open=boolean]`);
-  console.log(`example: coindockWsCmd localhost:6666 binance btcusdt 5min --limit=30 --open=true`);
-  process.exit(1);
+if (argv._.length < 4 || argv._.length > 6) {
+  usage();
 }
 
 const endpoint = argv._[0];
@@ -18,6 +16,10 @@ const interval = argv._[3];
 const limit = argv['limit'] != null ? Number(argv['limit']) : undefined;
 const open = argv['open'] != null ? (argv['open'] == 'true') : undefined;
 
+if (endpoint == null || exchange == null || symbol == null || interval == null) {
+  usage();
+}
+
 const coindock = new CoindockWs(endpoint);
 
 console.log(`using: ${endpoint}, sending: exchange=${exchange}, symbol=${symbol}, interval=${interval}, limit=${limit}, open=${open}`);
@@ -25,3 +27,9 @@ console.log(`using: ${endpoint}, sending: exchange=${exchange}, symbol=${symbol}
 coindock.onOhlcv(exchange, symbol, interval, limit, open, (msg) => {
   console.log(msg);
 });
+
+function usage() {
+  console.log(`usage: ./coindock-ws endpoint exchange symbol interval  [limit=number] [open=boolean]`);
+  console.log(`example: ./coindock-ws localhost:6666 binance btcusdt 5min --limit=30 --open=true`);
+  process.exit(1);
+}
