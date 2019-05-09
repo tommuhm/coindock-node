512305d7d9218ce641037cc9d6797f08a6089342
Tom Muhm <tom.muhm@gmail.com>
2019-05-09 16:44:02 +0200

    add open param to cmd tool

--- src/bin/coindockRestCmd.ts
+++ src/bin/coindockRestCmd.ts
@@ -18,18 +18,19 @@ const interval = argv._[4];
 const limit = argv['limit'] != null ? Number(argv['limit']) : undefined;
 const from = argv['from'] != null ? Number(argv['from']) : undefined;
 const to = argv['to'] != null ? Number(argv['to']) : undefined;
+const open = argv['open'] != null ? argv['open'] === 'true': false;
 const openLimit = argv['openLimit'] != null ? Number(argv['openLimit']) : undefined;
 
 if (endpoint == null || exchange == null || symbol == null || interval == null) {
   usage();
 }
 
-
 const coindock = new CoindockRest({endpoint, apiKey});
 
-console.log(`using: ${endpoint}, sending: exchange=${exchange}, symbol=${symbol}, interval=${interval}, limit=${limit}, from=${from}, to=${to}, openLimit=${openLimit}`);
+console.log(`using: ${endpoint}, sending: exchange=${exchange}, symbol=${symbol}, interval=${interval}, ` +
+ `limit=${limit}, from=${from}, to=${to}, open=${open} openLimit=${openLimit}`);
 
-coindock.ohlcv({exchange, symbol, interval, limit, from, to, openLimit}, (err, response) => {
+coindock.ohlcv({exchange, symbol, interval, limit, from, to, open, openLimit}, (err, response) => {
   if (err) {
     console.log(err);
   }
@@ -37,7 +38,7 @@ coindock.ohlcv({exchange, symbol, interval, limit, from, to, openLimit}, (err, r
 });
 
 function usage() {
-  console.log(`usage: ./coindock-rest endpoint apiKey exchange symbol interval [--from=timestampMs] [--to=timestampMs] [--limit=number] [--openLimit=number]`);
+  console.log(`usage: ./coindock-rest endpoint apiKey exchange symbol interval [--from=timestampMs] [--to=timestampMs] [--limit=number] [--open=boolean] [--openLimit=number]`);
   console.log(`example: ./coindock-rest localhost:5555 <YOUR-API-KEY> binance btcusdt 5min --from=1547392298000 --limit=500 --openLimit=30`);
   process.exit(1);
 }
